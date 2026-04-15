78440ced8630d2b63fdb77172636c80f459ecd25
RALC-TIE-CREATOR <rlecona1@gmail.com>
2026-04-15 00:49:14 -0600

    Update __init__.py

--- cpu/__init__.py
+++ cpu/__init__.py
@@ -1,7 +1,8 @@
 from .instrucciones import Instruccion, Operacion
 from .memoria import MemoriaPrograma, MemoriaDatos, CeldaMemoria
 from .cpu import CPU
- 
+
+
 def I(op, dest=None, src1=None, src2=None, label=None):
     """Shorthand para crear instrucciones."""
     return Instruccion(op, dest, src1, src2, label)
